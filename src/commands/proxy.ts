24647d256636d7e049e4aae50732095d1b5ae56a
Cafe137 <77121044+Cafe137@users.noreply.github.com>
2022-10-05 12:43:16 +0200

    feat: add hardcode stamp support to gateway proxy (#12)

--- src/commands/proxy.ts
+++ src/commands/proxy.ts
@@ -1,5 +1,7 @@
 import { Command, Parser } from 'cafe-args'
 import { Dates, System, Types } from 'cafe-utility'
+import { BEE_DEBUG_API_URL } from '../constants'
+import { findPostageStamp } from '../stamp'
 import { requireEnv } from '../utility'
 
 export function registerProxyCommand(parser: Parser) {
@@ -10,14 +12,20 @@ export function registerProxyCommand(parser: Parser) {
             .withOption({
                 key: 'stamp-autobuy',
                 type: 'boolean',
-                alias: 'b',
+                alias: 's',
                 description: 'Enable stamp autobuying'
             })
             .withOption({
                 key: 'stamp-autoextend',
                 type: 'boolean',
-                alias: 'x',
+                alias: 'S',
                 description: 'Enable stamp autoextending'
+            })
+            .withOption({
+                key: 'stamp-hardcoded',
+                type: 'boolean',
+                alias: 'h',
+                description: 'Enable hardcoded stamp'
             })
             .withOption({
                 key: 'hostname',
@@ -53,14 +61,20 @@ export function registerProxyCommand(parser: Parser) {
             .withOption({
                 key: 'preset0',
                 type: 'boolean',
-                alias: 'p',
+                alias: 'x',
                 description: 'Enables all features with autobuying'
             })
             .withOption({
                 key: 'preset1',
                 type: 'boolean',
-                alias: 'P',
+                alias: 'y',
                 description: 'Enables all features with autoextending'
+            })
+            .withOption({
+                key: 'preset2',
+                type: 'boolean',
+                alias: 'z',
+                description: 'Enables all features with hardcoded stamp'
             })
             .withOption({
                 key: 'debug',
@@ -71,10 +85,11 @@ export function registerProxyCommand(parser: Parser) {
             .withFn(async context => {
                 const location = requireEnv('CAFE_CLI_PROJECT_PROXY')
                 const hostname = context.options['hostname']
-                const all = context.options['preset0'] || context.options['preset1']
+                const all = context.options['preset0'] || context.options['preset1'] || context.options['preset2']
                 const stampAutobuy = context.options['stamp-autobuy'] || context.options.preset0
                 const stampAutoextend = context.options['stamp-autoextend'] || context.options.preset1
-                if (stampAutoextend && stampAutobuy) {
+                const stampHardcoded = context.options['stamp-hardcoded'] || context.options.preset2
+                if ([stampAutobuy, stampAutoextend, stampHardcoded].filter(x => x).length > 1) {
                     throw Error('Cannot enable both stamp autobuy and autoextend')
                 }
                 const ens = context.options['ens'] || all
@@ -87,14 +102,17 @@ export function registerProxyCommand(parser: Parser) {
                 if (stampAutobuy) {
                     env.POSTAGE_DEPTH = '22'
                     env.POSTAGE_AMOUNT = '200000'
-                    env.BEE_DEBUG_API_URL = 'http://localhost:1635'
+                    env.BEE_DEBUG_API_URL = BEE_DEBUG_API_URL
                 }
                 if (stampAutoextend) {
                     env.POSTAGE_DEPTH = '22'
                     env.POSTAGE_AMOUNT = '200000'
                     env.POSTAGE_TTL_MIN = '60'
-                    env.BEE_DEBUG_API_URL = 'http://localhost:1635'
+                    env.BEE_DEBUG_API_URL = BEE_DEBUG_API_URL
                     env.POSTAGE_EXTENDSTTL = 'true'
+                }
+                if (stampHardcoded) {
+                    env.POSTAGE_STAMP = (await findPostageStamp(BEE_DEBUG_API_URL)).batchID
                 }
                 if (hostname) {
                     env.HOSTNAME = Types.asString(hostname)
