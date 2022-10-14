e4a9ea384f0c8158e349631df7a249b2e51390ee
Cafe137 <xcafe137@gmail.com>
2022-10-14 14:17:56 +0200

    feat: add dilute support in fake-bee and proxy

--- src/commands/proxy.ts
+++ src/commands/proxy.ts
@@ -10,26 +10,38 @@ export function registerProxyCommand(parser: Parser) {
             alias: 'p'
         })
             .withOption({
-                key: 'stamp-autobuy',
+                key: 'dry-run',
                 type: 'boolean',
-                alias: 's',
-                description: 'Enable stamp autobuying'
+                alias: 'D',
+                description: 'Print env and exit'
             })
             .withOption({
-                key: 'stamp-autoextend',
+                key: 'autobuy',
+                type: 'boolean',
+                alias: 'a',
+                description: 'Enable stamp purchase'
+            })
+            .withOption({
+                key: 'topup',
+                type: 'boolean',
+                alias: 't',
+                description: 'Enable stamp topup'
+            })
+            .withOption({
+                key: 'dilute',
                 type: 'boolean',
                 alias: 'S',
-                description: 'Enable stamp autoextending'
+                description: 'Enable stamp dilution'
             })
             .withOption({
-                key: 'stamp-hardcoded',
+                key: 'hardcoded',
                 type: 'boolean',
                 alias: 'h',
                 description: 'Enable hardcoded stamp'
             })
             .withOption({
                 key: 'hostname',
-                alias: 'h',
+                alias: 'H',
                 description: 'Hostname of the proxy, required for Bzz.link',
                 default: 'bzz-link.local'
             })
@@ -60,20 +72,22 @@ export function registerProxyCommand(parser: Parser) {
             .withOption({
                 key: 'preset0',
                 type: 'boolean',
-                alias: 'x',
-                description: 'Enables all features with autobuying'
+                description: 'Enables all features with autobuy'
             })
             .withOption({
                 key: 'preset1',
                 type: 'boolean',
-                alias: 'y',
-                description: 'Enables all features with autoextending'
+                description: 'Enables all features with topup'
             })
             .withOption({
                 key: 'preset2',
                 type: 'boolean',
-                alias: 'z',
-                description: 'Enables all features with hardcoded stamp'
+                description: 'Enables all features with dilute'
+            })
+            .withOption({
+                key: 'preset3',
+                type: 'boolean',
+                description: 'Enables all features with hardcoded'
             })
             .withOption({
                 key: 'debug',
@@ -85,32 +99,39 @@ export function registerProxyCommand(parser: Parser) {
                 const location = requireEnv('CAFE_CLI_PROJECT_PROXY')
                 const hostname = context.options['hostname']
                 const all = context.options['preset0'] || context.options['preset1'] || context.options['preset2']
-                const stampAutobuy = context.options['stamp-autobuy'] || context.options.preset0
-                const stampAutoextend = context.options['stamp-autoextend'] || context.options.preset1
-                const stampHardcoded = context.options['stamp-hardcoded'] || context.options.preset2
-                if ([stampAutobuy, stampAutoextend, stampHardcoded].filter(x => x).length > 1) {
-                    throw Error('Cannot enable both stamp autobuy and autoextend')
+                const autobuy = context.options['autobuy'] || context.options.preset0
+                const topup = context.options['topup'] || context.options.preset1
+                const dilute = context.options['dilute'] || context.options.preset2
+                const hardcoded = context.options['hardcoded'] || context.options.preset3
+                if ([autobuy, topup, hardcoded, dilute].filter(x => x).length > 1) {
+                    throw Error('Exclusive options: autobuy, topup, hardcoded, dilute')
                 }
                 const ens = context.options['ens'] || all
                 const cid = context.options['cid'] || all
                 const reupload = context.options['reupload'] || all
+                const debug = context.options['debug'] || all
+                const quick = context.options['quick'] || all
                 const env: Record<string, string> = {
                     PATH: Types.asString(process.env.PATH),
                     EXPOSE_HASHED_IDENTITY: 'true'
                 }
-                if (stampAutobuy) {
+                if (autobuy) {
                     env.POSTAGE_DEPTH = '22'
                     env.POSTAGE_AMOUNT = '200000'
                     env.BEE_DEBUG_API_URL = BEE_DEBUG_API_URL
                 }
-                if (stampAutoextend) {
+                if (topup) {
                     env.POSTAGE_DEPTH = '22'
                     env.POSTAGE_AMOUNT = '200000'
                     env.POSTAGE_TTL_MIN = '60'
                     env.BEE_DEBUG_API_URL = BEE_DEBUG_API_URL
                     env.POSTAGE_EXTENDSTTL = 'true'
                 }
-                if (stampHardcoded) {
+                if (dilute) {
+                    env.BEE_DEBUG_API_URL = BEE_DEBUG_API_URL
+                    env.POSTAGE_EXTENDS_CAPACITY = 'true'
+                }
+                if (hardcoded) {
                     env.POSTAGE_STAMP = (await findPostageStamp(BEE_DEBUG_API_URL)).batchID
                 }
                 if (hostname) {
@@ -125,16 +146,19 @@ export function registerProxyCommand(parser: Parser) {
                 if (reupload) {
                     env.REUPLOAD_PERIOD = Dates.minutes(2).toString()
                 }
-                if (context.options['debug']) {
+                if (debug) {
                     env.LOG_LEVEL = 'debug'
                 }
-                if (context.options['quick']) {
+                if (quick) {
                     env.POSTAGE_REFRESH_PERIOD = Dates.seconds(2).toString()
                     if (reupload) {
                         env.REUPLOAD_PERIOD = Dates.seconds(2).toString()
                     }
                 }
-                console.log('Starting with env:', env)
+                console.log(env)
+                if (context.options['dry-run']) {
+                    return
+                }
                 System.runProcess(
                     'npm',
                     ['start'],
